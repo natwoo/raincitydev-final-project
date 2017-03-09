c07d4f04e32e5ce777f8c3302bfeab862a19c52c
Andrew Hwang <ahwang10@uw.edu>
2017-03-09 13:05:41 -0800

    finish

--- server.R
+++ server.R
@@ -1,17 +1,62 @@
 library(shiny)
 library(ggplot2)
-library(leaflet)
 library(dplyr)
-library(stringr)
 library(rgdal)
+library(maps)
+library(leaflet)
+library(tidyr)
 library(htmltools)
 library(htmlwidgets)
-library(tidyr)
 
 revenue.data <- read.csv("data/washington_school_district_level_revenue.csv", stringsAsFactors = FALSE)
 msp.hspe.data <- read.csv("data/2010_to_2013_MSP_HSPE_Scores_by_District.csv", stringsAsFactors = FALSE)
+# reads in shapefile of school district boundaries
+school.district.shapefile <- readOGR("./data/shapefiles", "tl_2016_53_unsd", stringsAsFactors = FALSE) %>% 
+  rmapshaper::ms_simplify()
 
 server <- function(input, output) {
+# Reactives
+    map.data <- reactive({
+    temp <- school.district.shapefile
+    revenue.map.data <- mutate(revenue.data, NAME = district, year = paste0(substr(year, 1, 2), substr(year, 6, 7))) %>% 
+      select(NAME, year, total.revenue.per.pupil) %>% 
+      filter(year == input$years.slider) %>% 
+      spread(year, total.revenue.per.pupil)
+    colnames(revenue.map.data) <- c("NAME", "total.revenue.per.pupil")
+    temp@data <- left_join(temp@data, revenue.map.data)
+    temp
+  })
+# Renders
+  output$map <- renderLeaflet({
+    
+    
+    pal <- colorNumeric(palette = "Blues", domain = school.district.shapefile@data$total.revenue.per.pupil)
+    
+    map <- leaflet(map.data()) %>% 
+      addTiles() %>%
+      addEasyButton(easyButton(
+        icon="fa-globe", title="Zoom to Level 1",
+        onClick=JS("function(btn, map){ map.setZoom(1); }"))) %>%
+      addEasyButton(easyButton(
+        icon="fa-crosshairs", title="Locate Me",
+        onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
+    
+    map <- map %>%
+      addPolygons(weight = 1,
+                  fillColor = ~pal(total.revenue.per.pupil),
+                  fillOpacity = 0.8,
+                  label = ~paste0(NAME," (Revenue per Pupil: $", total.revenue.per.pupil, ")")) %>% 
+      addLegend("bottomright",
+                pal = pal, values = ~total.revenue.per.pupil,
+                title = "Total Revenue Per Pupil",
+                labFormat = labelFormat(prefix = "$"),
+                opacity = 1
+      )
+    
+    
+    
+  })
+  
   
 }
 
--- ui.R
+++ ui.R
@@ -1,53 +1,40 @@
 library(shiny)
-library(ggplot2)
+library(plotly)
 library(leaflet)
 
 ui <- fluidPage(
   #Title
   titlePanel("Title TBA"),
-  
+
   br(),
-  
   # A two panel layout with sidebar and main panel
   sidebarLayout(
     # Sidebar
     sidebarPanel(
       radioButtons("grades.button", "Grade of Interest:",
-                   c("6th Grade" = "6", 
-                     "7th Grade" = "7", 
-                     "8th Grade" = "8", 
-                     "10th Grade" = "10")),
+                   c("6th Grade" = "sixth", 
+                     "7th Grade" = "seventh", 
+                     "8th Grade" = "eighth", 
+                     "10th Grade" = "tenth")),
       
       br(),
-      
-      radioButtons("subject.check", "Test Subject:",
-                   c("Reading" = "reading",
-                     "Math" = "math",
-                     "Writing" = "writing",
-                     "Science" = "science")),
-      
-      br(),
-      
-      selectInput("school.district", "School Districts:",
-                  districts),
-      
-      br(),
-      
       sliderInput("years.slider", "Years:",
                   min = 2010,
                   max = 2013,
                   value = 2013,
                   step = 1,
-                  sep = "")
+                  format="####")
     ),
     
     # Main panel
     mainPanel(
       # Create tabs
+      leafletOutput("map"),
+      br(),
       tabsetPanel(type = "tabs",
                   tabPanel("Revenue vs. Test Score"),
                   tabPanel("Data Table"),
-                  tabPanel("Per District Test Proficiency Distribution", plotOutput("plot2"), br(), textOutput("plot2.description"))
+                  tabPanel("Aggregated Test Data")
       )
     )
   )
